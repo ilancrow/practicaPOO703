6ffdeaf0fdfdbf184fc8e1c413946109a85db5a6
jose daniel <josdapaniagua@gmail.com>
2022-02-23 17:12:38 -0500

    se da bienvenida a OASIS

--- practicaPOO703/Program.cs
+++ practicaPOO703/Program.cs
@@ -6,7 +6,7 @@ namespace practicaPOO703
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bienvenido a OASIS ");
         }
     }
 }
