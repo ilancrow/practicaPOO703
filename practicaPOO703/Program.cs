5cebfaba1f4374660c7bbd5ed1464bca15f19dd5
jose daniel <josdapaniagua@gmail.com>
2022-02-23 16:50:50 -0500

    Agregar archivos de proyecto.

---
+++ practicaPOO703/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace practicaPOO703
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
